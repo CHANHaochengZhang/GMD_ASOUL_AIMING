ad8be003f06e6e11a63318137e569c94fae41cf0
1999Chen <lsy1999lsy@gmail.com>
2022-05-06 13:34:24 +0200

    yangtuo script added

---
+++ Assets/Scripts/YangtuoController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class YangtuoController : MonoBehaviour
+{
+  private static event Action<YangtuoController> onEnemyKilled;
+  private float health, maxHealth = 10f;
+  private float moveSpeed = 5f;
+  private Transform target;
+  private Vector2 moveDirection;
+
+   void Start()
+   {
+       health = maxHealth;
+       Debug.Log("health is "+health);
+       health = 1;
+       Debug.Log("health is "+health);
+       
+   }
+
+   public void takeDamage(float damageAmount)
+   {
+       Debug.Log("Damage taken from player: "+damageAmount);
+       health = health-damageAmount;
+       Debug.Log("Current health: "+health);
+       if (health<=0)
+       {
+           Destroy(gameObject);
+           onEnemyKilled?.Invoke(this);
+       }
+   }
+}
